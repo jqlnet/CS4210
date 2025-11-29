134263448c8bffa9c4874e864f807ed262058237
jqlnet <toniliang@cpp.edu>
2025-11-28 18:43:29 -0800

    added assignment4

---
+++ Assignment4/CNN.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Toni Liang    
+# FILENAME: CNN 
+# SPECIFICATION: 
+# Complete the Python program (cnn.py) that will train a Convolutional Neural Network
+# (CNN) using TensorFlow and Keras to classify 32x32 images of handwritten digits (0 through 9). Your
+# task is to 1) load the dataset using the provided helper function, 2) normalize and reshape the input data
+# for the CNN, 3) construct a CNN architecture following the guidelines in the code, 4) train the model
+# on the training set and validate on the test set, 5) evaluate the final model and 6) print the test accuracy.
+# FOR: CS 4210- Assignment #4
+# TIME SPENT: 3h35m
+#-----------------------------------------------------------*/
